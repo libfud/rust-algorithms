64b9fc78e04e32acd1f5815f862f260dd46c858f
Dominick Allen <djallen@librehumanitas.org>
2014-03-23 08:55:55 -0400

    Added a date parsing function

--- common/records.rs
+++ common/records.rs
@@ -8,17 +8,17 @@ pub struct Complex_Number {
 
 /// A date is composed of an integer day, month and year.
 pub struct Date {
-    day: int,
-    month: int,
-    year: int
+    day: uint,
+    month: uint,
+    year: uint
 }
 
 /// A person's record is composed of their family name, first name,
 /// date of birth, their sex and marital status
 pub struct Person {
-    family_name: ~str,
-    first_name : ~str,
-    DOB: ~str, // Temporary til I find out how structs in structs work
-    Is_Female  : bool,
-    Is_Married : bool
+    family_name   : ~str,
+    first_name    : ~str,
+    date_of_birth : Date,
+    Is_Female     : bool,
+    Is_Married    : bool
 }
