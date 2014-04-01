79ae46ba8110bfb6f1f2d6ba27703cca153ab417
libfud <libfud@librehumanitas.org>
2014-03-31 23:41:20 -0400

    tempconv iteration 2

--- tempconv.rs
+++ tempconv.rs
@@ -17,17 +17,32 @@ fn cels_to_fahr(cels: f64) -> f64 {
 }
 
 fn main() {
+
     let args = std::os::args();
-    if args.len() < 2 { 
-        println(":^o");
-        return;
-    }
+    let help_str = 
+    "Type a number followed by f or F to signify Fahrenheit to convert it to\n"
+    +"Celsius. Use c or C to convert to Celsius.";
 
-    let (temperature, scale) = parse_string_to_float(args[1].to_owned());
-
-    match scale.slice_to(1){
-        "F"|"f" => println!("{}C", fahr_to_cels(temperature)),
-        "C"|"c" => println!("{}F", cels_to_fahr(temperature)),
-        _ => println("Woops")
+    if args.len() > 1 {
+        let argument = args[1].to_owned();
+        if argument.len() >= 2 {
+            if argument.slice_to(2) == "-h" {
+                println!("{}", help_str);
+                return;
+            }
+        }
+        let (temperature, scale) = parse_string_to_float(args[1].to_owned());
+        if scale.len() > 0 {
+            match scale.slice_to(1){
+                "F"|"f" => println!("{}C", fahr_to_cels(temperature)),
+                "C"|"c" => println!("{}F", cels_to_fahr(temperature)),
+                _ => println("use tempconv -h for help")
+            }
+        }
+        else {
+            println("use tempconv -h for help");
+            return;
+        }
     }
+    else { println("use tempconv -h for help") }
 }
