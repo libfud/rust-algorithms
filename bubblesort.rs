acbd235b2abf65d14cdcbe5d6cffc6f5c1dd3e37
libfud <libfud@librehumanitas.org>
2014-04-01 20:24:10 -0400

    refactored bubblesort

--- bubblesort.rs
+++ bubblesort.rs
@@ -1,3 +1,8 @@
+#[crate_id = "bubblesort"];
+#[crate_type = "bin"];
+
+//!An implementation of bubblesort.
+
 use common::utils::int_array_from_file;
 
 pub mod common { pub mod utils; }
@@ -20,9 +25,15 @@ fn bubble_sort(array_orig: ~[int]) -> ~[int] {
 }
 
 fn main() {
-    let array_size = 100000;
-    let pathname = ~"common/numbers.txt";
-    let array = int_array_from_file(pathname, array_size);
+    
+    let args = std::os::args();
+    if args.len() < 2 {
+        println("I need a filename.");
+        return;
+    }
+
+    let pathname = args[1].to_owned();
+    let array = int_array_from_file(pathname);
     let sorted_array = bubble_sort(array);
     for &elem in sorted_array.iter() {
         println!("{}",elem);
