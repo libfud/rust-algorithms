6c3ea39919724172d7f214d13074f2270095d612
libfud <libfud@librehumanitas.org>
2014-04-03 21:21:53 -0400

    refactoring code

--- linear_search.rs
+++ linear_search.rs
@@ -1,41 +1,45 @@
-#[crate_id = "linear_search"];
-#[crate_type = "bin"];
+#![crate_id = "linear_search"]
+#![crate_type = "bin"]
 
 //!An implementation of a linear search.
+
+use common::utils::string_getter;
 
 pub mod common {
     pub mod utils;
 }
 
 fn main() {
-    let mut array_size = common::utils::number_getter("What is the number of "+
-    "elements in the array?");
-    if array_size < 1 {
-        println!("Using 10.");
-        array_size = 10;
-    }
-    let mut upper_bound = common::utils::number_getter("Input a factor to "+
-    "determine the largest number possible in the array.");
-    if upper_bound < 1 { upper_bound = 10; }
-    let array = common::utils::array_gen(array_size,upper_bound);
+    let array_size = match from_str::<uint>(string_getter("What is the number of "+
+    "elements in the array?")) {
+        Some(num) => num,
+        _         => 10, //default size
+    };
+    let factor = match from_str::<uint>(string_getter("What is the factor by "+
+    "which you will use for the range?")) {
+        Some(num) => num,
+        _         => 2, //default factor
+    };
+
+    let guess = match from_str::<uint>(string_getter("What is the number" +
+    " you are looking for?")) {
+        Some(num) => num,
+        _         => 1,
+    };
+
+    let array = common::utils::array_gen(array_size, factor);
+
+    let mut i = 0;
 
     loop {
-        let z = common::utils::number_getter("Input a number to search for "
-        +"between 1 and the factor times the number of elements in the array.");
-        if (z > 0 && z < (10*array_size+1)) { y = z; break; }
-    }
-
-    println!("{}",array[array_size-1]);
-    
-    let mut i = 0;
-    while i < array_size-1 {
-        if y == array[i] { break }
-        println!("{}",array[i]);
+        if i == array_size - 1 { break }
+        if array[i] == guess { break }
         i += 1;
     }
-    
-    if array[i]== y {
-        println!("Array index {} holds value {} equal to the supplied value of {}", i, array[i], y);
+
+    if array[i] == guess {
+        println!("Array element {} holds {}, which equals {}",
+            i, array[i], guess);
     }
-    else { println("Sorry, didn't find it."); }
+    else { println!("Did not find it."); }
 }
