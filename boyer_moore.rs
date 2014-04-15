2799d1767cd5958841d0cce79a6e7d1c6a6cd960
libfud <libfud@librehumanitas.org>
2014-04-15 05:54:58 -0400

    fixed boyer moore :)

--- boyer_moore.rs
+++ boyer_moore.rs
@@ -2,9 +2,11 @@
 #![crate_type = "bin"]
 
 extern crate getopts;
+extern crate collections;
 use getopts::{reqopt, optflag,getopts,OptGroup};
+use collections::HashMap;
 use std::os;
-use common::utils::array_from_file;
+use common::utils::{array_from_file};
 
 pub mod common { 
     pub mod utils;
@@ -17,10 +19,16 @@ fn print_usage(program: &str, _opts: &[OptGroup]) {
 }
 
 fn boyer_moore_search(array: &[~str], key: &str) -> (bool, uint, uint) {
-    if array.len() < 1 { return (false, 0u, 0u) }
+    if array.len() < 1 || key.len() < 1 { return (false, 0u, 0u) }
+    let bad_table = bad_char_table_gen(key);
     let mut match_pos: uint;
     let mut found: bool;
     let mut i = 0u;
+    let mut keylen = 0u;
+
+    for _ in key.chars() {
+        keylen += 1;
+    }
 
     loop {
         match_pos = 0;
@@ -28,21 +36,20 @@ fn boyer_moore_search(array: &[~str], key: &str) -> (bool, uint, uint) {
         if i >= array.len() { break }
         let string_i = array[i].clone();
         let str_len = string_i.len() - 1;
+
         loop {
-            let mut jump = 1u;
-            let right_index = match_pos + key.len();
-            if right_index  >= str_len { break }
-            if string_i[match_pos] == key[0] {
-                jump = reverse_search(string_i.slice(match_pos,
-                    match_pos+key.len()),key);
-            }
-            if jump == 0 { 
+            let right_i = match_pos + key.len();
+            if right_i  > str_len { break }
+            let (found_inner,jump) = reverse_search(
+                string_i.slice(match_pos, right_i), key, keylen,
+                bad_table.clone());
+            if found_inner == true {
                 found = true;
                 break;
-            } else { 
-                match_pos += key.len() - jump;
             }
+            match_pos += jump;
         }
+
         if found == true { break }
         i += 1;
     }
@@ -50,14 +57,40 @@ fn boyer_moore_search(array: &[~str], key: &str) -> (bool, uint, uint) {
     return (found, i, match_pos);
 }
 
-fn reverse_search(string_i: &str, key: &str) -> uint {
-    let mut i = key.len() - 1;
-    while i < key.len() {
-        if string_i[i] != key[i] { break }
+fn reverse_search(stringa: &str,key: &str, keylen: uint,
+    table: HashMap<char,uint>) -> (bool, uint) {
+    let mut i = keylen;
+    let mut j = 1u;
+    let mut found = false;
+    for (chara, charb) in key.chars_rev().zip(stringa.chars_rev()) {
+        if chara != charb {
+            j  = match table.find(&charb) {
+                Some(jump_value)    => jump_value.clone(),
+                _                   => j
+            }; 
+            break;
+        }
         i -= 1;
     }
-    if i > key.len() { i = 0 }
-    return i
+    if i == 0  { found = true }
+    if j < i + 1 { return (found, j) }
+    return (found, i + 1) 
+}
+
+fn bad_char_table_gen(key: &str) -> HashMap<char, uint> {
+    let mut bad_table = HashMap::new();
+    let length = key.len();
+    let mut i = 0u;
+    for eachc in key.chars()  {
+        let jump = length - 1 - i;
+        if jump > length || jump == 0 {
+            bad_table.insert_or_update_with(eachc, 1, |_k, v| *v = 1);
+        } else {
+            bad_table.insert_or_update_with(eachc, jump, |_k, v| *v = jump);
+        }
+        i += 1;
+    }
+    return bad_table;
 }
 
 fn main() {
