0ac8b91a1da23636492e7bf6ce5645f84bb134bf
libfud <libfud@librehumanitas.org>
2014-04-15 06:08:07 -0400

    boyer moore: added comments and documentation

--- boyer_moore.rs
+++ boyer_moore.rs
@@ -1,6 +1,8 @@
 #![crate_id = "boyer_moore"]
 #![crate_type = "bin"]
 
+//!Boyer Moore string search, simplified. No good suffix rule here.
+
 extern crate getopts;
 extern crate collections;
 use getopts::{reqopt, optflag,getopts,OptGroup};
@@ -8,9 +10,7 @@ use collections::HashMap;
 use std::os;
 use common::utils::{array_from_file};
 
-pub mod common { 
-    pub mod utils;
-}
+pub mod common { pub mod utils; }
 
 fn print_usage(program: &str, _opts: &[OptGroup]) {
     println!("Usage: {} [options]", program);
@@ -18,36 +18,42 @@ fn print_usage(program: &str, _opts: &[OptGroup]) {
     println!("-k ,\t--key\t\t Key value");
 }
 
+///Takes an array of strings and a key, returns a boolean to indicate if the
+///string was found, the index of the string in which it was found, and the
+///index of that string at which it was found.
 fn boyer_moore_search(array: &[~str], key: &str) -> (bool, uint, uint) {
     if array.len() < 1 || key.len() < 1 { return (false, 0u, 0u) }
     let bad_table = bad_char_table_gen(key);
     let mut match_pos: uint;
     let mut found: bool;
-    let mut i = 0u;
+    let mut i = 0u; //i is the index of the string in the array
     let mut keylen = 0u;
 
     for _ in key.chars() {
         keylen += 1;
     }
+    //There's probably a method to do this better.
 
     loop {
+        //match position is the index of the string at which the key is
+        //found. Resets to 0 for each new sring in the array.
         match_pos = 0;
         found = false;
         if i >= array.len() { break }
         let string_i = array[i].clone();
-        let str_len = string_i.len() - 1;
+        let str_len = string_i.len() - 1; //no out of bounds access
 
         loop {
-            let right_i = match_pos + key.len();
-            if right_i  > str_len { break }
-            let (found_inner,jump) = reverse_search(
+            let right_i = match_pos + key.len(); //we're slicing strings
+            if right_i  > str_len { break } //no oob
+            let (found_inner, jump) = reverse_search(
                 string_i.slice(match_pos, right_i), key, keylen,
                 bad_table.clone());
             if found_inner == true {
                 found = true;
                 break;
             }
-            match_pos += jump;
+            match_pos += jump; //advance the index at which we slice
         }
 
         if found == true { break }
@@ -57,13 +63,18 @@ fn boyer_moore_search(array: &[~str], key: &str) -> (bool, uint, uint) {
     return (found, i, match_pos);
 }
 
+///Reverse iterates through the two strings; the key and the string
+///we're searching. Returns whether or not we found a match (i == 0)
+///and an unsigned integer to advance the slicing point.
 fn reverse_search(stringa: &str,key: &str, keylen: uint,
     table: HashMap<char,uint>) -> (bool, uint) {
-    let mut i = keylen;
-    let mut j = 1u;
+    let mut i = keylen; //don't want to mutate the length of the key
+    let mut j = 1u; 
     let mut found = false;
+    //reverse iterate through  the key and the substring
     for (chara, charb) in key.chars_rev().zip(stringa.chars_rev()) {
         if chara != charb {
+            //j gets assigned the appropriate jump value from the table
             j  = match table.find(&charb) {
                 Some(jump_value)    => jump_value.clone(),
                 _                   => j
@@ -73,10 +84,17 @@ fn reverse_search(stringa: &str,key: &str, keylen: uint,
         i -= 1;
     }
     if i == 0  { found = true }
+    //indicates a complete loop through all values in the key and string
     if j < i + 1 { return (found, j) }
+    //add one to i because if j got assigned a value then i did not
+    //get incremented
     return (found, i + 1) 
+    //this happens if the prior comparison was false
 }
 
+///generate a hashmap mapping a uint value to each char in the key
+///depending on how many instances of the char are in the key and
+///the location of the char in the key
 fn bad_char_table_gen(key: &str) -> HashMap<char, uint> {
     let mut bad_table = HashMap::new();
     let length = key.len();
